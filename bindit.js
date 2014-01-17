51ada56eb99fd5794308bd81c0a51e82acae7f36
Dmitry Balakov <dmitry@balakov.net>
2014-01-17 12:49:54 +0400

    Fix

--- bindit.js
+++ bindit.js
@@ -819,7 +819,7 @@
     return result;
   };
 
-  Template.OftenAttributes = ['id', 'name', 'class', 'style', 'src', 'href', 'value', BindIt.DATA_BIND_ATTRIBUTE, BindIt.VIEW_ATTRIBUTE, 'item-view'];
+  Template.OftenAttributes = ['id', 'name', 'class', 'style', 'src', 'href', 'value', BindIt.DATA_BIND_ATTRIBUTE, BindIt.VIEW_ATTRIBUTE, 'item-view', 'type'];
 
   Template.TEMPLATE_ID_PROPERTY = 'template-id';
 
